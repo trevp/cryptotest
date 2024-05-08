6de43cc5227f5180e02ce37b276b201399647c07
Trevor Perrin <trevp@trevp.net>
2024-05-08 10:43:08 -0700

    3 proof examples

--- Cryptotest/RRtoLR.lean
+++ Cryptotest/RRtoLR.lean
@@ -29,11 +29,27 @@ def oracle_right (s: SymEnc KeyType MessageType CiphertextType)
 def is_lr_ind (s: SymEnc KeyType MessageType CipherTextType) :=
   ∀ (k: KeyType) (ml: MessageType) (mr: MessageType), oracle_left s k ml mr = oracle_right s k ml mr
 
+-- 3 proof examples:
 
-theorem ror_implies_lr (s: SymEnc KeyType MessageType CipherTextType) :
+theorem ror_implies_lr_proof_1 (s: SymEnc KeyType MessageType CipherTextType) :
     is_ror_ind s → is_lr_ind s := by
-  -- simp_all [oracle_real, oracle_random, is_ror_ind, oracle_left, oracle_right, is_lr_ind, SymEnc]
-  -- above works, but do it very explicitly for demonstration:
+  simp_all [oracle_real, oracle_random, is_ror_ind, oracle_left, oracle_right, is_lr_ind, SymEnc]
+
+theorem ror_implies_lr_proof_2 (s: SymEnc KeyType MessageType CipherTextType) :
+    is_ror_ind s → is_lr_ind s := by
+  unfold is_lr_ind
+  unfold oracle_left oracle_right
+  intro h
+  unfold is_ror_ind at h
+  unfold oracle_real oracle_random at h
+  intro k ml mr
+  -- prove goal: ∀ k ml mlr, s.enc k ml = s.enc k mr
+  -- using h:    ∀ k m, s.enc k m = s.random ()
+  calc s.enc k ml = s.random () := by rw [(h k ml)]
+                _ = s.enc k mr  := by rw [← (h k mr)]
+
+theorem ror_implies_lr_proof_3 (s: SymEnc KeyType MessageType CipherTextType) :
+    is_ror_ind s → is_lr_ind s := by
   intro h
   unfold is_lr_ind
   unfold oracle_left oracle_right
