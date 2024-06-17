d7b85998efcba794197df4b95d31b3854c64e1bc
Trevor Perrin <trevp@trevp.net>
2024-06-16 17:50:20 -0700

    Elgamal, initial_func

--- Cryptotest/Bits.lean
+++ Cryptotest/Bits.lean
@@ -8,14 +8,13 @@ inductive Bits where
   | rand_pub      : Bits     -- indistinguishable from random but public (e.g. nonce)
   | num (n: Nat)  : Bits     -- integer encoded into bits
 
-namespace Bits
-@[simp] def xor (b1: Bits) (b2: Bits) : Bits :=
+@[simp] def bits_xor (b1: Bits) (b2: Bits) : Bits :=
   match b1, b2 with
-  | rand, _ => rand -- XOR of random with anything is random
-  | _, rand => rand
-  | _, _    => any  -- Otherwise "any"
-end Bits
-open Bits
+  | Bits.rand, _ => Bits.rand -- XOR of random with anything is random
+  | _, Bits.rand => Bits.rand
+  | _, _      => Bits.any  -- Otherwise "any"
+
+@[simp] instance : Add Bits where add := bits_xor -- overload "+" operator
 
 -- This PRF will always return rand if evaluated on rand or rand_pub inputs.
 structure PrfRandInputs
@@ -23,12 +22,12 @@ structure PrfRandInputs
 namespace PrfRandInputs
 
 @[simp] def new : Bits → PrfRandInputs × Bool
-  | rand => ({}, true)
+  | Bits.rand => ({}, true)
   | _ => ({}, false)
 
 @[simp] def eval (prf: PrfRandInputs) : Bits →  Bits × PrfRandInputs
-  | rand | rand_pub => (rand, prf)
-  | _ => (any, prf)
+  | Bits.rand | Bits.rand_pub => (Bits.rand, prf)
+  | _ => (Bits.any, prf)
 
 end PrfRandInputs
 
@@ -39,92 +38,142 @@ structure PrfNumInputs where
 namespace PrfNumInputs
 
 @[simp] def new : Bits → PrfNumInputs × Bool
-  | rand => ({used_nums := {}}, true)
+  | Bits.rand => ({used_nums := {}}, true)
   | _ => ({used_nums := {}}, false)
 
 @[simp] def eval (prf: PrfNumInputs) : Bits → Bits × PrfNumInputs
-  | num n =>
-    if n ∉ prf.used_nums then (rand, {prf with used_nums := prf.used_nums ∪ {n} })
-    else (any, prf)
-  | _ => (any, prf)
+  | Bits.num n =>
+    if n ∉ prf.used_nums then
+      (Bits.rand, {prf with used_nums := prf.used_nums ∪ {n} })
+    else
+      (Bits.any, prf)
+  | _ => (Bits.any, prf)
 
 end PrfNumInputs
 
 -- Simple proofs: Encryption with One-Time Pad
 ----------------------------------------------
-@[simp] def enc_otp         : Bits := xor rand any
-@[simp] def enc_double_otp  : Bits := xor rand (xor rand any)
+@[simp] def enc_otp         : Bits := Bits.rand + Bits.any
+@[simp] def enc_double_otp  : Bits := Bits.rand + (Bits.rand + Bits.any)
 
-theorem is_ind_enc_otp        : enc_otp = rand        := by simp
-theorem is_ind_enc_double_otp : enc_double_otp = rand := by simp
+theorem is_ind_enc_otp        : enc_otp = Bits.rand        := by rfl
+theorem is_ind_enc_double_otp : enc_double_otp = Bits.rand := by rfl
 
 -- CPA security of simple PRF encryption scheme
 -----------------------------------------------
-structure EncryptionScheme (EncStateType: Type) where
-  initial_state : EncStateType
-  enc_func : EncStateType → (Bits × EncStateType)
+structure EncryptionScheme (EncState: Type) where
+  initial_func : Bits → EncState          -- initialize with a key
+  enc_func : EncState → (Bits × EncState) -- encrypt Bits.any
 
 -- Security definition
-@[simp] def is_cpa (scheme: EncryptionScheme EncStateType) : Prop :=
-  ∃ (sec_invariant: EncStateType → Prop),
-      (sec_invariant scheme.initial_state) ∧
-      ∀ (s: EncStateType), sec_invariant s →
-        (scheme.enc_func s).1 = rand ∧
+@[simp] def is_cpa (scheme: EncryptionScheme EncState) : Prop :=
+  let initial_state := scheme.initial_func Bits.rand
+  ∃ (sec_invariant: EncState → Prop),
+      (sec_invariant initial_state) ∧
+      ∀ (s: EncState), sec_invariant s →
+        (scheme.enc_func s).1 = Bits.rand ∧
         sec_invariant (scheme.enc_func s).2
 
 -- The simple encryption scheme: r=rand, prf(k,r) xor msg
 @[simp] def enc_prf_random : EncryptionScheme PrfRandInputs :=
-  {initial_state := (PrfRandInputs.new rand).1,
-   enc_func := fun prf => let (prf_out, prf_next) := prf.eval rand_pub
-                          (xor prf_out any, prf_next)}
+  {initial_func := fun bits => (PrfRandInputs.new bits).1,
+   enc_func := fun prf => let (prf_out, prf_next) := prf.eval Bits.rand_pub
+                          (prf_out + Bits.any, prf_next)}
 
 -- Security proof
 theorem is_cpa_enc_prf_random: is_cpa enc_prf_random := by
   use fun _ => True  -- security invariant (True for all inputs)
-  simp
+  aesop
 
 -- CPA security of nonce-based PRF encryption scheme
 -- Adversary allowed to choose nonces n, but can't reuse them.
 -- Similar to Easycrypt: https://fdupress.gitlab.io/easycrypt-web/
 --------------------------------------------------------------
-structure EncryptionSchemeWithNonce (EncStateType: Type) where
-  initial_state : EncStateType
-  enc_func : EncStateType → Nat → (Bits × EncStateType)
+structure EncryptionSchemeWithNonce (EncState: Type) where
+  initial_func : Bits → EncState                -- initialize with a key
+  enc_func : EncState → Nat → (Bits × EncState) -- encrypt Bits.any with nonce
 
-variable (scheme: EncryptionSchemeWithNonce EncStateType)
+variable (scheme: EncryptionSchemeWithNonce EncState)
 
 -- The adversary interacts with a stateful game:
-structure NRGameState (EncStateType: Type) where
-  enc_state : EncStateType
+structure NRGameState (EncState: Type) where
+  enc_state : EncState
   used_nums : Finset Nat
 
 -- Initialize the game state
-@[simp] def nr_game_init : NRGameState EncStateType := ⟨scheme.initial_state, {}⟩
+@[simp] def nr_game_init : NRGameState EncState :=
+  ⟨scheme.initial_func Bits.rand, {}⟩
 
 -- The adversary calls this oracle with nonce n
-@[simp] def nr_game_oracle (gs: NRGameState EncStateType) (n : Nat) :
-    Bits × (NRGameState EncStateType) :=
-  if n ∈ gs.used_nums then (rand, gs) -- adversary tried to cheat
+@[simp] def nr_game_oracle (gs: NRGameState EncState) (n : Nat) :
+    Bits × (NRGameState EncState) :=
+  if n ∈ gs.used_nums then (Bits.rand, gs) -- adversary tried to cheat
   else
     let (enc_out, s) := scheme.enc_func gs.enc_state n
     (enc_out, {enc_state := s, used_nums := gs.used_nums ∪ {n} })
 
 -- Security definition
 @[simp] def is_nr_cpa : Prop :=
-  ∃ (sec_invariant: NRGameState EncStateType → Prop),
+  ∃ (sec_invariant: NRGameState EncState → Prop),
       (sec_invariant (nr_game_init scheme)) ∧
-      ∀ (gs: NRGameState EncStateType), sec_invariant gs →
+      ∀ (gs: NRGameState EncState), sec_invariant gs →
         ∀ (n: Nat),
-          (nr_game_oracle scheme gs n).1 = rand ∧
+          (nr_game_oracle scheme gs n).1 = Bits.rand ∧
           sec_invariant (nr_game_oracle scheme gs n).2
 
 -- The simple encryption scheme: n=nonce, prf(k,n) xor msg
 @[simp] def enc_prf_nonce : EncryptionSchemeWithNonce PrfNumInputs :=
-  {initial_state := (PrfNumInputs.new rand).1,
-   enc_func := fun prf n => let (prf_out, prf_next) := prf.eval (num n)
-                            (xor prf_out any, prf_next)}
+  {initial_func := fun bits => (PrfNumInputs.new bits).1,
+   enc_func := fun prf n => let (prf_out, prf_next) := prf.eval (Bits.num n)
+                            (prf_out + Bits.any, prf_next)}
 
 -- Security proof
 theorem is_nr_cpa_enc_prf_nonce: is_nr_cpa enc_prf_nonce := by
   use fun gs => gs.used_nums = gs.enc_state.used_nums -- security invariant
   aesop
+
+-- Now moving to public-key crypto and DH
+-----------------------------------------
+inductive GroupElement where
+  | any           : GroupElement     -- any value
+  | rand          : GroupElement     -- indistinguishable from random
+  | rand_pub      : GroupElement     -- indistinguishable from random but public (e.g. nonce)
+  | entropy       : GroupElement     -- high entropy but not necessarily uniform
+  | num (n: Nat)  : GroupElement     -- scalar n encoded into group element via G^n
+
+@[simp] def group_element_add (e1: GroupElement) (e2: GroupElement) :=
+  match e1, e2 with
+  | GroupElement.rand, _  => GroupElement.rand
+  | _, GroupElement.rand  => GroupElement.rand
+  | _, _                  => GroupElement.any
+
+@[simp] instance : Add GroupElement where
+  add := group_element_add -- overload "+" operator
+
+@[simp] def hash_to_bits: GroupElement → Bits
+  | GroupElement.rand | GroupElement.entropy => Bits.rand
+  | _ => Bits.any
+
+@[simp] def ddh_triple: (GroupElement × GroupElement × GroupElement) :=
+  (GroupElement.rand_pub, GroupElement.rand_pub, GroupElement.rand)
+
+@[simp] def cdh_triple: (GroupElement × GroupElement × GroupElement) :=
+  (GroupElement.rand_pub, GroupElement.rand_pub, GroupElement.entropy)
+
+-- Simple proofs: ElGamal and Hashed ElGamal Encryption
+--------------------------------------------------------
+@[simp] def elgamal_ddh: GroupElement :=
+  let (_pub_key, _pub_ephemeral, shared_secret) := ddh_triple
+  shared_secret + GroupElement.any
+
+@[simp] def hashed_elgamal_ddh: Bits :=
+  let (_pub_key, _pubephemeral, shared_secret) := ddh_triple
+  (hash_to_bits shared_secret) + Bits.any
+
+@[simp] def hashed_elgamal_cdh: Bits :=
+  let (_pub_key, _pub_ephemeral, shared_secret) := cdh_triple
+  (hash_to_bits shared_secret) + Bits.any
+
+theorem is_one_time_ind_elgamal_ddh : elgamal_ddh = GroupElement.rand := by rfl
+theorem is_one_time_ind_hashed_elgamal_ddh : hashed_elgamal_ddh = Bits.rand := by rfl
+theorem is_one_time_ind_hashed_elgamal_cdh : hashed_elgamal_cdh = Bits.rand := by rfl
